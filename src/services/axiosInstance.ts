3e451c50b19064b4344fb4a0e7a1229836be311e
Semih Yonet <semih@shoppingminds.com>
2024-08-11 19:30:37 +0200

    Created base Axios instance

---
+++ src/services/axiosInstance.ts
@@ -0,0 +1,32 @@
+import axios, {AxiosInstance, AxiosError, AxiosResponse} from "axios";
+
+const axiosClient = (token: string | null = null): AxiosInstance => {
+    // const headers = {
+    //     "Content-Type": "multipart/form-data",
+    // };
+    const baseURL = process.env.BASEURL || "http://localhost:8000";
+    const client = axios.create({
+        baseURL: baseURL,
+        // headers,
+        timeout: 60000,
+        withCredentials: false,
+    });
+
+    client.interceptors.request.use((config: any) => {
+        config.headers = config.headers || {};
+        return config;
+    });
+
+    client.interceptors.response.use(
+        (response: AxiosResponse) => {
+            return response;
+        },
+        (error: AxiosError) => {
+            throw error;
+        }
+    );
+
+    return client;
+};
+
+export default axiosClient;
